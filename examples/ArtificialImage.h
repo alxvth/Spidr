a136b639847b6b89c7592745d609abd52de2d05f
Alexander Vieth <a.vieth@tudelft.nl>
2021-02-17 18:19:14 +0100

    Add necessary files for example

---
+++ examples/ArtificialImage.h
@@ -0,0 +1,47 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+
+template<typename T>
+std::vector<T> loadData(const std::string fileName)
+{
+	std::vector<T> fileContents;
+
+	// open file 
+	std::ifstream istrm(fileName, std::ios::in | std::ios::binary);
+	if (!istrm.is_open()) {
+		throw std::invalid_argument("Unable to load file: " + fileName);
+	}
+	else {
+		// number of data points
+		istrm.seekg(0, istrm.end);
+		auto N = istrm.tellg();		
+		istrm.seekg(0, istrm.beg);
+
+		// read data
+		fileContents.resize(N * sizeof(T));
+		istrm.read(reinterpret_cast<char*>(fileContents.data()), fileContents.size() * sizeof(T));
+		istrm.close();
+	}
+
+	return fileContents;
+}
+
+int main() {
+	const std::string fileName = "data/CheckeredBoxes_2Ch_32.bin";
+
+	std::vector<float> data = loadData<float>(fileName);
+
+	// Settings
+
+
+	// Compute embedding
+
+	// Save embedding
+
+    return 0;
+}
+
---
+++ src/TsneComputation.cpp
@@ -0,0 +1,272 @@
+#include "TsneComputation.h"
+
+#include "AnalysisParameters.h"
+#include "EvalUtils.h"
+
+#include <algorithm>            // std::min, max
+#include <vector>
+#include <assert.h>
+
+#include "hdi/dimensionality_reduction/tsne_parameters.h"
+#include "hdi/utils/scoped_timers.h"
+#include "spdlog/spdlog-inl.h"
+
+
+
+// Change this to glad and glfw as in https://github.com/biovault/nptsne
+class OffscreenBuffer : public QWindow
+{
+public:
+    OffscreenBuffer()
+    {
+        setSurfaceType(QWindow::OpenGLSurface);
+
+        create();
+    }
+
+    QOpenGLContext* getContext() { return _context; }
+
+    void initialize()
+    {
+        QOpenGLContext* globalContext = QOpenGLContext::globalShareContext();
+        _context = new QOpenGLContext(this);
+        _context->setFormat(globalContext->format());
+
+        if (!_context->create())
+            qFatal("Cannot create requested OpenGL context.");
+
+        _context->makeCurrent(this);
+        if (!gladLoadGL()) {
+            qFatal("No OpenGL context is currently bound, therefore OpenGL function loading has failed.");
+        }
+    }
+
+    void bindContext()
+    {
+        _context->makeCurrent(this);
+    }
+
+    void releaseContext()
+    {
+        _context->doneCurrent();
+    }
+
+private:
+    QOpenGLContext* _context;
+};
+
+OffscreenBuffer* offBuffer;
+
+TsneComputation::TsneComputation() :
+_iterations(1000),
+_numTrees(4),
+_numChecks(1024),
+_exaggerationIter(250),
+_exponentialDecay(250),
+_perplexity(30), 
+_perplexity_multiplier(3),
+_numDimensionsOutput(2),
+_verbose(false),
+_isGradientDescentRunning(false),
+_isTsneRunning(false),
+_isMarkedForDeletion(false),
+_continueFromIteration(0)
+{
+    _nn = _perplexity * _perplexity_multiplier + 1;
+}
+
+TsneComputation::~TsneComputation()
+{
+}
+
+void TsneComputation::computeGradientDescent()
+{
+    initGradientDescent();
+
+    embed();
+}
+
+void TsneComputation::setup(const std::vector<int> knn_indices, const std::vector<float> knn_distances, const Parameters params) {
+    // Parameters
+    _iterations = params._numIterations;
+    _perplexity = params._perplexity;
+    _exaggerationIter = params._exaggeration;
+    _exponentialDecay = params._expDecay;
+    _nn = params._nn;                       // same as in constructor = _perplexity * 3 + 1;
+    _numPoints = params._numPoints;
+    _perplexity_multiplier = params._perplexity_multiplier;
+
+    // Evaluation (for determining the filename when saving the embedding to disk)
+    _embeddingName = params._embeddingName;
+    _numDataDims = params._numDims;
+
+    // Data
+    _knn_indices = knn_indices;
+    _knn_distances = knn_distances;
+
+	spdlog::info("t-SNE computation: Num data points: {0} with {1} precalculated nearest neighbors. Perplexity: {2}, Iterations: {3}", _numPoints, params._nn, _perplexity, _iterations);
+
+    assert(_knn_indices.size() == _numPoints * _nn);
+}
+
+
+void TsneComputation::initTSNE()
+{
+        
+    // Computation of the high dimensional similarities
+    {
+        hdi::dr::HDJointProbabilityGenerator<float>::Parameters probGenParams;
+        probGenParams._perplexity = _perplexity;
+        probGenParams._perplexity_multiplier = _perplexity_multiplier;
+        probGenParams._num_trees = _numTrees;
+        probGenParams._num_checks = _numChecks;
+
+		spdlog::info("tSNE initialized.");
+
+        _probabilityDistribution.clear();
+        _probabilityDistribution.resize(_numPoints);
+		spdlog::info("Sparse matrix allocated.");
+
+        hdi::dr::HDJointProbabilityGenerator<float> probabilityGenerator;
+        double t = 0.0;
+        {
+            hdi::utils::ScopedTimer<double> timer(t);
+            probabilityGenerator.computeGaussianDistributions(_knn_distances, _knn_indices, _nn, _probabilityDistribution, probGenParams);
+        }
+		spdlog::info("Probability distributions calculated.");
+		spdlog::info("================================================================================");
+		spdlog::info("A-tSNE: Compute probability distribution: {} seconds", t / 1000);
+		spdlog::info("--------------------------------------------------------------------------------");
+    }
+}
+
+void TsneComputation::initGradientDescent()
+{
+    _continueFromIteration = 0;
+
+    _isTsneRunning = true;
+
+    hdi::dr::TsneParameters tsneParams;
+
+    tsneParams._embedding_dimensionality = _numDimensionsOutput;
+    tsneParams._mom_switching_iter = _exaggerationIter;
+    tsneParams._remove_exaggeration_iter = _exaggerationIter;
+    tsneParams._exponential_decay_iter = _exponentialDecay;
+    tsneParams._exaggeration_factor = 4 + _numPoints / 60000.0;
+    _A_tSNE.setTheta(std::min(0.5, std::max(0.0, (_numPoints - 1000.0)*0.00005)));
+
+    // Create a context local to this thread that shares with the global share context
+    offBuffer = new OffscreenBuffer();
+    offBuffer->initialize();
+
+    // Initialize GPGPU-SNE
+    offBuffer->bindContext();
+    _GPGPU_tSNE.initialize(_probabilityDistribution, &_embedding, tsneParams);
+    
+    copyFloatOutput();
+}
+
+// Computing gradient descent
+void TsneComputation::embed()
+{
+    double elapsed = 0;
+    double t = 0;
+    {
+		spdlog::info("A-tSNE: Computing gradient descent..\n");
+        _isGradientDescentRunning = true;
+
+        // Performs gradient descent for every iteration
+        for (int iter = 0; iter < _iterations; ++iter)
+        {
+            hdi::utils::ScopedTimer<double> timer(t);
+            if (!_isGradientDescentRunning)
+            {
+                _continueFromIteration = iter;
+                break;
+            }
+
+            // Perform a GPGPU-SNE iteration
+            _GPGPU_tSNE.doAnIteration();
+
+            if (iter > 0 && iter % 10 == 0)
+            {
+                copyFloatOutput();
+            }
+
+            if (t > 1000)
+				spdlog::info("Time: {}", t);
+
+            elapsed += t;
+        }
+        offBuffer->releaseContext();
+
+        copyFloatOutput();
+        
+        _isGradientDescentRunning = false;
+        _isTsneRunning = false;
+
+    }
+
+	spdlog::info("--------------------------------------------------------------------------------");
+	spdlog::info("A-tSNE: Finished embedding of tSNE Analysis in: {} seconds", elapsed / 1000);
+	spdlog::info("================================================================================");
+
+}
+
+void TsneComputation::compute() {
+    initTSNE();
+    computeGradientDescent();
+}
+
+// Copy tSNE output to our output
+void TsneComputation::copyFloatOutput()
+{
+    _outputData = _embedding.getContainer();
+}
+
+const std::vector<float>& TsneComputation::output()
+{
+    return _outputData;
+}
+
+void TsneComputation::setVerbose(bool verbose)
+{
+    _verbose = verbose;
+}
+
+void TsneComputation::setIterations(int iterations)
+{
+    _iterations = iterations;
+}
+
+void TsneComputation::setExaggerationIter(int exaggerationIter)
+{
+    _exaggerationIter = exaggerationIter;
+}
+
+void TsneComputation::setExponentialDecay(int exponentialDecay)
+{
+    _exponentialDecay = exponentialDecay;
+}
+
+void TsneComputation::setPerplexity(int perplexity)
+{
+    _perplexity = perplexity;
+}
+
+void TsneComputation::setNumDimensionsOutput(int numDimensionsOutput)
+{
+    _numDimensionsOutput = numDimensionsOutput;
+}
+
+void TsneComputation::stopGradientDescent()
+{
+    _isGradientDescentRunning = false;
+}
+
+void TsneComputation::markForDeletion()
+{
+    _isMarkedForDeletion = true;
+
+    stopGradientDescent();
+}
